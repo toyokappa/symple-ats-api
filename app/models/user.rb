cdf37bf6db164504e34523cb22fdf4e91d8a9e35
Yuta Toyokawa <kppg42@gmail.com>
2021-12-04 01:16:54 +0900

    invitableの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
   enum role: { viewer: 10, interviewer: 20, admin: 30 }
