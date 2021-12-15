ef7e0e46adaab4db30f7fc789340c0a16ed30420
Yuta Toyokawa <kppg42@gmail.com>
2021-12-15 18:51:12 +0900

    カンバンの移動時に採用履歴も作成されるように修正

--- app/controllers/candidates/positions_controller.rb
+++ app/controllers/candidates/positions_controller.rb
@@ -3,6 +3,11 @@ class Candidates::PositionsController < ApplicationController
     candidate = Candidate.find_by(id: params[:candidate_id])
     return render status: 404 if candidate.blank?
 
+    # 選考履歴が必要な分作成する
+    params[:require_selection_ids]&.each do |selection_id|
+      candidate.recruitment_histories.find_or_initialize_by(recruitment_selection_id: selection_id)
+    end
+
     candidate.update(candidate_params)
     render json: CandidateBlueprint.render(candidate, view: :normal)
   end
