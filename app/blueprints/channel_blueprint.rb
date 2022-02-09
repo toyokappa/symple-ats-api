17c0d44e7e5c3d1e4414ebc713b0163085b16d08
Yuta Toyokawa <kppg42@gmail.com>
2022-02-09 21:51:03 +0900

    推薦用トークン吐き出しロジックの実装

--- app/blueprints/channel_blueprint.rb
+++ app/blueprints/channel_blueprint.rb
@@ -2,6 +2,6 @@ class ChannelBlueprint < Blueprinter::Base
   identifier :id
 
   view :normal do
-    fields :name, :category, :automation, :apply_token
+    fields :name, :category, :automation, :apply_token, :organization_unique_id
   end
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -13,10 +13,24 @@
 #
 class Channel < ApplicationRecord
   belongs_to :organization
+  delegate :unique_id, to: :organization, prefix: true
 
   enum category: { agent: 10, ad: 20, scout: 30, sns: 40, referral: 50, other: 60 }, _prefix: true
   enum automation: { disable: 10, enable: 20, impossible: 30 }, _prefix: true
 
   validates :name, presence: true
   validates :category, presence: true
+
+  before_save :generate_apply_token
+
+  private
+
+  def generate_apply_token
+    return if apply_token.present? || !category_agent?
+
+    self.apply_token = loop do
+      tmp_token = SecureRandom.alphanumeric(24)
+      break tmp_token unless self.class.find_by(apply_token: tmp_token)
+    end
+  end
 end
