a696a2b9bbea99125b8633bc33048ca3021e0878
Yuta Toyokawa <kppg42@gmail.com>
2022-02-14 18:09:56 +0900

    選考履歴のスペックを追加

--- spec/models/recruitment_history_spec.rb
+++ spec/models/recruitment_history_spec.rb
@@ -19,4 +19,13 @@ RSpec.describe RecruitmentHistory, type: :model do
       expect(history).to be_valid
     end
   end
+
+  describe "#update_auto_scheduling_token" do
+    let(:history) { create :recruitment_history }
+    it "スケジュール調整用のトークンが作成される" do
+      expect(history.auto_scheduling_token.present?).to eq false
+      history.update_auto_scheduling_token
+      expect(history.auto_scheduling_token.present?).to eq true
+    end
+  end
 end
