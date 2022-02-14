f5da3fe62d3b99499ff118fa874f95b9667a2b63
Yuta Toyokawa <kppg42@gmail.com>
2022-02-14 19:04:07 +0900

    プロジェクトのスペックを追加

--- spec/models/recruitment_project_spec.rb
+++ spec/models/recruitment_project_spec.rb
@@ -19,16 +19,38 @@ RSpec.describe RecruitmentProject, type: :model do
   end
 
   describe "chart" do
+    let(:project) { create :recruitment_project }
+    before do
+      create :recruitment_selection, recruitment_project: project, selection_type: :document, name: '書類選考'
+      create :recruitment_selection, recruitment_project: project, selection_type: :interview, name: '1次面接'
+      create :recruitment_selection, recruitment_project: project, selection_type: :interview, name: '2次面接'
+      create :recruitment_selection, recruitment_project: project, selection_type: :offer, name: '内定'
+      create :recruitment_selection, recruitment_project: project, selection_type: :consent, name: '内定承諾'
+      create :recruitment_selection, recruitment_project: project, selection_type: :failure, name: '見送り'
+      create :recruitment_selection, recruitment_project: project, selection_type: :decline, name: '辞退'
+      create :recruitment_selection, recruitment_project: project, selection_type: :other, name: 'その他'
+    end
+
     describe "#chart_labels" do
+      it "グラフのラベルが返却される" do
+        expect(project.chart_labels).to eq ['書類選考', '1次面接', '2次面接', '内定', '内定承諾', '']
+      end
     end
 
     describe "#chart_values" do
+      # TODO: ロジックが複雑なので後回し
     end
 
-    describe "#chart_datasets" do
+    describe "#to_table_headers" do
+      # TODO: ロジックが複雑なので後回し
     end
 
-    describe "#chart_data" do
+    describe "#to_table_values" do
+      # TODO: ロジックが複雑なので後回し
+    end
+
+    describe "#chart_json" do
+      # TODO: ロジックが複雑なので後回し
     end
   end
 end
